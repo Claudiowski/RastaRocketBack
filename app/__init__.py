9969460d1c5116e4bea917e8d032cb5e8db97f8a
Arthur Verdier <a.verdier@outlook.fr>
2017-10-03 19:44:45 +0200

    try cors

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,13 @@ def create_app(config_name='default'):
 
     extensions(app)
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
+
     return app
 
 
